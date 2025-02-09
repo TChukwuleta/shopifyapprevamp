735b0a4b3f7393c7b26d85c8c5cad48502eb1140
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2025-02-09 12:37:52 -0800

    change file

---
+++ app/routes/shopifyapp._index.jsx
@@ -0,0 +1,9 @@
+// app/routes/shopifyapp._index.tsx
+export default function ShopifyApp() {
+    return (
+      <div>
+        <h1>Welcome to the Shopify App</h1>
+        <p>This is the Shopify app loaded on /shopifyapp.</p>
+      </div>
+    );
+  }
--- app/routes/shopifyapp.app.jsx
+++ app/routes/shopifyapp.app.jsx
@@ -19,10 +19,10 @@ export default function App() {
   return (
     <AppProvider isEmbeddedApp apiKey={apiKey}>
       <NavMenu>
-        <Link to="/app" rel="home">
+        <Link to="/shopifyapp/app" rel="home">
           Home
         </Link>
-        <Link to="/app/additional">Additional page</Link>
+        <Link to="/shopifyapp/app/additional">Additional page</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
